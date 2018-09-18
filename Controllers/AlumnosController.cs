b6ed7c49130997028cfd1f38468043fe32d28220
= <josecarlos.wms@gmail.com>
2018-09-18 23:22:50 +0200

    first commit

---
+++ Controllers/AlumnosController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            var db = new ColegioEntities1();
+            
+            return View(db.Alumnos.ToList());
+        }
+
+        public ActionResult CreateAlumn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAlumn(Alumnos a)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                var db = new ColegioEntities1();
+                a.fecha_registro = DateTime.Now;
+                db.Alumnos.Add(a);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("","Error: "+ex.Message);
+                return View();
+            }
+         
+        }
+
+        public ActionResult EditAlumno(int id)
+        {
+            using (var db = new ColegioEntities1())
+            {
+                return View(db.Alumnos.Find(id));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditAlumno(Alumnos a)
+        {
+            if (!ModelState.IsValid) return View();
+            using (var db = new ColegioEntities1())
+            {
+                var al = db.Alumnos.Find(a.id);
+                al.nombre = a.nombre;
+                al.apellido1 = a.apellido1;
+                al.edad = a.edad;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult DetailsAlumn(int id)
+        {
+            using (var db = new ColegioEntities1())
+            {
+                var al = db.Alumnos.Find(id);
+                return View(al);
+            }
+        }
+
+        public ActionResult DeleteAlumn(int id)
+        {
+            using (var db = new ColegioEntities1())
+            {
+                var al = db.Alumnos.Find(id);
+                db.Alumnos.Remove(al);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
---
+++ Models/Alumnos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2
+{
+    public class AlumnosCE
+    {
+        public int id { get; set; }
+        [Required]
+        [Display(Name = "Inserte nombre")]
+        public string nombre { get; set; }
+        [Required]
+        public string apellido1 { get; set; }
+        [Required]
+        [MinLength(1)]
+        [MaxLength(3)]
+        public string edad { get; set; }
+        public System.DateTime fecha_registro { get; set; }
+    }
+
+    // Si no indicamos aquí metadatatype no funcionarán las validaciones.
+    [MetadataType(typeof(AlumnosCE))]
+    public partial class Alumnos
+    {
+        public int Estado { get; set; }
+        public string NombreCompleto { get { return nombre + " " + apellido1;  } }
+    }
+}
